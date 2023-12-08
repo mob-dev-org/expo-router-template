a60b5a5f9884ed581fd0efca61a3cf1ebcd84975
Mahir Kadić <kadic.mahir.pro@gmail.com>
2023-12-08 10:41:56 +0100

    feat(reanimated version): reanimated version
    git push

---
+++ app/home.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export type indexProps = {};
+
+const index = (props: indexProps) => {
+    return (
+        <View style={styles.container}>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+            <Text>home component</Text>
+        </View>
+    );
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+    container: {},
+});
